787e1fe483e43bf9e76e343f980bf8a8dc2f0a88
Stella Chung <schung507@gmail.com>
2016-11-27 12:06:08 +0000

    fixed some syntax

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -24,11 +24,6 @@ class MicropostsController < ApplicationController
     @post = Micropost.find_by_id(params[:id])
     # handle any errors from the code above
     @mpost = Micropost.all
-    if params[:search]
-      @posts = Post.search(params[:search]).order("created_at DESC")
-    else
-      @posts = Post.all.order('created_at DESC')
-  end
   end
 
   private

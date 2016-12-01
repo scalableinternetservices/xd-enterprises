b4ccfc8c79483a6d1810eacd5046d79d7e34c06d
Stella Chung <schung507@gmail.com>
2016-12-01 07:10:29 +0000

    fixed syntax

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -10,7 +10,7 @@ class Micropost < ActiveRecord::Base
   validates :image, presence: true
   
   acts_as_commontable
-  has_attached_file :image, :styles => { :medium => "400x400>", :thumb => "100x100>" }
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates_attachment_content_type :image , :content_type => /\Aimage\/.*\Z/
   include SimpleHashtag::Hashtaggable
   hashtaggable_attribute :content

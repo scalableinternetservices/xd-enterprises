d0fd9fa6026892a2b1eea3f3e93c4079a32fc88c
Stella Chung <schung507@gmail.com>
2016-12-01 06:54:25 +0000

    fixed key

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -1,5 +1,6 @@
 module MicropostsHelper
    def cache_key_for_post(micropost)
-      "post-#{Commontator::Thread.find(micropost.id).comments.ids.count}"
+      "post-#{micropost.id}-#{micropost.updated_at}-#{Commontator::Thread.find(micropost.id).comments.ids.count}"
+      p  "post-#{micropost.id}-#{micropost.updated_at}-#{Commontator::Thread.find(micropost.id).comments.ids.count}"
    end
 end
